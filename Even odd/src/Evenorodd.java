54403999c12034e61f6059e692af548949a85ee8
admin <admin@DESKTOP-L8D5R7T>
2022-06-23 23:52:12 +0530

    Even or odd

---
+++ Even odd/src/Evenorodd.java
@@ -0,0 +1,19 @@
+
+public class Evenorodd {
+
+	public static void main(String[] args) {
+		int num=666;
+		boolean flag =((num&1)==0)?true:false;
+		if(flag)
+		{
+			System.out.println(+num+"is Even number");
+		}
+		else
+		{
+			System.out.println(+num+"is Odd number");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
