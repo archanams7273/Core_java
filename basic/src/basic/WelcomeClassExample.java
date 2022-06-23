d14500c2d8791c5cf699b1df29ad2e33676cd070
admin <admin@DESKTOP-L8D5R7T>
2022-06-23 23:53:04 +0530

    Basic hello

---
+++ basic/src/basic/WelcomeClassExample.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class WelcomeClassExample {
+
+	public static void main(String[] args) {
+		System.out.println("helloworld");
+		// TODO Auto-generated method stub
+
+	}
+
+}
